a5cea5d3946aa507163d023191d87a152ca7f99f
Luke Lohden <lukasl@mac.com>
2017-01-24 18:39:39 -0800

    Added my early prototype work on the game and a gitignore!

---
+++ Sake Sushi Chef/Assets/Scripts/GamePlay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlay : MonoBehaviour {
+
+	public static string boardSlot1 = "empty";
+	public static string boardSlot2 = "empty";
+	public static string boardSlot3 = "empty";
+
+	public static string grillSlot1 = "empty";
+	public static string grillSlot2 = "empty";
+	public static string grillSlot3 = "empty";
+
+	public static int selectedSlot = 0;
+	public static int selectedFood = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Sake Sushi Chef/Assets/Scripts/ServeFood.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServeFood : MonoBehaviour {
+
+	public int occupiedSlot = 0;
+	public bool mouseControlled = false;
+
+	// Use this for initialization
+	void Start () {
+		occupiedSlot = GamePlay.selectedSlot;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mouseControlled) {
+			Vector2 mouseposition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+			Vector2 objPosition = Camera.main.ScreenToWorldPoint (mouseposition);
+			transform.position = objPosition;
+		}
+	}
+
+	void OnMouseDown(){
+		mouseControlled = true;
+		GamePlay.selectedFood = occupiedSlot;
+		Debug.Log (mouseControlled);
+	}
+}
