13843183086c44ffc1fbec0c475e0e8801405ee5
Jerrid Domingo <JerridD@uci.edu>
2017-01-31 10:17:17 -0800

    Started Board.cs script

---
+++ Sake Sushi Chef/Assets/Scripts/Board.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class board : MonoBehaviour {
+    GameObject cutting_board;
+    private GameObject[] ingredients;
+    private const int NUM_INGREDIENTS = 9;
+	// Use this for initialization
+	void Start () {
+        ingredients = new GameObject[NUM_INGREDIENTS];
+        for (int i = 0; i < NUM_INGREDIENTS; i++){
+            ingredients[i] = null;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
