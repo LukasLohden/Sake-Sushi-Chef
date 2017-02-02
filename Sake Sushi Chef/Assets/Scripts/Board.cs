4af0d757dda8b5a7b49c7c7277445a412d1d77d4
Jerrid Domingo <JerridD@uci.edu>
2017-02-02 14:04:14 -0800

    Added ingredient selection status, dev feat. Harry, Eric

--- Sake Sushi Chef/Assets/Scripts/Board.cs
+++ Sake Sushi Chef/Assets/Scripts/Board.cs
@@ -1,23 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class board : MonoBehaviour {
-    GameObject tuna;
-    GameObject yellow_tail;
-    GameObject avacado;
-    GameObject shrimp;
-    GameObject crab;
-    GameObject rice;
-    GameObject nori;
-    GameObject teriyaki;
-    GameObject chilli_oil;
+public class Board : MonoBehaviour {
+    public GameObject tuna;
+    public GameObject yellow_tail;
+    public GameObject avacado;
+    public GameObject shrimp;
+    public GameObject crab;
+    public GameObject rice;
+    public GameObject nori;
+    public GameObject teriyaki;
+    public GameObject chilli_oil;
 
-    GameObject cutting_board;
+    public GameObject board_tuna;
+    public GameObject board_yellow_tail;
+    public GameObject board_avacado;
+    public GameObject board_shrimp;
+    public GameObject board_crab;
+    public GameObject board_rice;
+    public GameObject board_nori;
+    public GameObject board_teriyaki;
+    public GameObject board_chilli_oil;
+
+    public GameObject cutting_board;
     private GameObject[] ingredients;
     private const int NUM_INGREDIENTS = 9;
+    private int ingredient_count;
 	// Use this for initialization
 	void Start () {
         ingredients = new GameObject[NUM_INGREDIENTS];
+        ingredient_count = 2;
         for (int i = 0; i < NUM_INGREDIENTS; i++){
             ingredients[i] = null;
         }
@@ -28,4 +40,43 @@ public class board : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    public void showIngredient (GameObject g) {
+        if (g==tuna)
+        {
+            board_tuna.GetComponent<SpriteRenderer>().sortingOrder = ingredient_count++;
+        }
+        else if(g == yellow_tail)
+        {
+            board_yellow_tail.GetComponent<SpriteRenderer>().sortingOrder = ingredient_count++;
+        }
+        else if (g == nori)
+        {
+            board_nori.GetComponent<SpriteRenderer>().sortingOrder = ingredient_count++;
+        }
+        else if (g == rice)
+        {
+            board_rice.GetComponent<SpriteRenderer>().sortingOrder = ingredient_count++;
+        }
+        else if (g == avacado)
+        {
+            board_avacado.GetComponent<SpriteRenderer>().sortingOrder = ingredient_count++;
+        }
+        else if (g == shrimp)
+        {
+            board_shrimp.GetComponent<SpriteRenderer>().sortingOrder = ingredient_count++;
+        }
+        else if (g == crab)
+        {
+            board_crab.GetComponent<SpriteRenderer>().sortingOrder = ingredient_count++;
+        }
+        else if (g == chilli_oil)
+        {
+            board_chilli_oil.GetComponent<SpriteRenderer>().sortingOrder = ingredient_count++;
+        }
+        else if (g == teriyaki)
+        {
+            board_teriyaki.GetComponent<SpriteRenderer>().sortingOrder = ingredient_count++;
+        }
+    }
 }
--- Sake Sushi Chef/Assets/Scripts/Ingredients.cs
+++ Sake Sushi Chef/Assets/Scripts/Ingredients.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 
 public class Ingredients : MonoBehaviour {
-    
+
     private int cost_to_purchase = 0;
     private int cost_to_sell = 0;
-    private bool active = false;
-
+    //private bool active = false;
+    public GameObject cutting_board;
 	// Use this for initialization
 	void Start () {
-	
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    
 	}
+    void OnMouseDown()
+    {
+        cutting_board.SendMessage("showIngredient", gameObject);
+    }
+
 }
